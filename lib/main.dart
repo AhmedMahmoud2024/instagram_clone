7dbce12d80158e0fbf105f0a246a083c419dceee
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2024-12-15 15:42:35 -0800

    implemented login screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter/screens/login_screen.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:instagram_flutter/firebase_options.dart';
@@ -19,14 +20,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Instagram clone',
-        theme: ThemeData.dark()
-            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-        home: const ResponsiveLayout(
-            webScreenLayout: WebScreenLayout(),
-            mobileScreenLayout: MobileScreenLayout())
-        // Scaffold(body: Text("Lets build instagram clone")));
-        );
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram clone',
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: const LoginScreen(),
+      //   home: const ResponsiveLayout(
+      //    webScreenLayout: WebScreenLayout(),
+      //    mobileScreenLayout: MobileScreenLayout())
+    );
   }
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            //svg picture
+            SvgPicture.asset(
+              "assets/ic_instagram.svg",
+              color: primaryColor,
+              height: 64,
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            //text field input for email
+            TextFieldInput(
+                textEditingController: _emailController,
+                hintText: "Enter your email",
+                textInputType: TextInputType.emailAddress),
+            const SizedBox(
+              height: 24,
+            ),
+            //text field input for password
+            TextFieldInput(
+              textEditingController: _passwordController,
+              hintText: "Enter your password",
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            //button login
+            InkWell(
+              onTap: () {},
+              child: Container(
+                child: const Text('Log in'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4))),
+                  color: blueColor, // Move the color here inside decoration
+                ),
+                // Remove the separate color property
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Flexible(
+              child: Container(),
+              flex: 2,
+            ),
+            //transition to signup
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: const Text("Dont have an account?"),
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                ),
+                SizedBox(
+                  width: 6,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
