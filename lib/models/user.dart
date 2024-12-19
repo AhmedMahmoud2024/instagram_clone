4aba7c825aefe89ca80ee874f7303237ef7c0d80
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2024-12-19 15:59:25 -0800

    i fixed issue on implemented of user data state management

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,52 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
+  final String? email;
+  final String? uid;
+  final String? photoUrl;
+  final String? username;
+  final String? bio;
+  final List<String>? followers;
+  final List<String>? following;
+
+  const User({
+    this.username,
+    this.uid,
+    this.photoUrl,
+    this.email,
+    this.bio,
+    this.followers,
+    this.following,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "username": username ?? '',
+        "uid": uid ?? '',
+        "email": email ?? '',
+        "photoUrl": photoUrl ?? '',
+        "bio": bio ?? '',
+        "followers": followers ?? [],
+        "following": following ?? [],
+      };
+
+  static User fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return User(
+      username: snapshot["username"] ?? '',
+      uid: snapshot["uid"] ?? '',
+      email: snapshot["email"] ?? '',
+      photoUrl: snapshot["photoUrl"] ?? '',
+      bio: snapshot["bio"] ?? '',
+      followers: List<String>.from(snapshot["followers"] ?? []),
+      following: List<String>.from(snapshot["following"] ?? []),
+    );
+  }
+}
+/*
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
   final String email;
   final String uid;
   final String photoUrl;
@@ -31,12 +77,13 @@ class User {
   static User fromSnap(DocumentSnapshot snap) {
     var snapShot = snap.data() as Map<String, dynamic>;
     return User(
-        email: snapShot['email'],
-        uid: snapShot['uid'],
-        photoUrl: snapShot['photoUrl'],
         username: snapShot['username'],
+        uid: snapShot['uid'],
+        email: snapShot['email'],
+        photoUrl: snapShot['photoUrl'],
         bio: snapShot['bio'],
         followers: snapShot['followers'],
         following: snapShot['following']);
   }
 }
+*/
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_flutter/models/user.dart' as model;
 import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:instagram_flutter/models/user.dart' as model;
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -18,7 +18,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
     model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
       body: Center(
-        child: Text(user.username ?? 'No username'),
+        child: Text(user.bio ?? 'email not  found'),
       ),
     );
   }
